eb5e90717ad30bd858ea05ed6a7276ed262eae8a
kimi5231 <djwjod@naver.com>
2023-09-28 20:10:42 +0900

    change sprit sheet and animation code

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,7 +7,6 @@ character = load_image('sprite_sheet.png')
 
 def handle_events():
     global running
-    global x, y
     global dirX, dirY
     global frameY
     events = get_events()
@@ -19,34 +18,38 @@ def handle_events():
                 running = False
             elif event.key == SDLK_LEFT:
                 frameY = 2
-                dirX = -1
+                dirX -= 1
             elif event.key == SDLK_RIGHT:
                 frameY = 0
-                dirX = 1
+                dirX += 1
             elif event.key == SDLK_UP:
-                frameY = 3
-                dirY = 1
-            elif event.key == SDLK_DOWN:
                 frameY = 1
-                dirY = -1
+                dirY += 1
+            elif event.key == SDLK_DOWN:
+                frameY = 3
+                dirY -= 1
         elif event.type == SDL_KEYUP:
-            if event.key == SDLK_LEFT or event.key == SDLK_RIGHT:
-                dirX = 0
-            elif event.key == SDLK_UP or event.key == SDLK_DOWN:
-                dirY = 0
+            if event.key == SDLK_LEFT:
+                dirX += 1
+            elif event.key == SDLK_RIGHT:
+                dirX -= 1
+            elif event.key == SDLK_UP:
+                dirY -= 1
+            elif event.key == SDLK_DOWN:
+                dirY += 1
 
 running = True
-frameX, frameY = 0, 1
+frameX, frameY = 0, 3
 x, y = WIDTH//2, HEIGHT//2
 dirX, dirY = 0, 0
 
 while(running):
     clear_canvas()
     background.draw(WIDTH//2, HEIGHT//2)
-    character.clip_draw(frameX * 64, frameY * 64, 64, 64, x, y)
+    character.clip_draw(frameX * 96, frameY * 104, 96, 104, x, y)
     update_canvas()
     handle_events()
-    frameX = (frameX + 1) % 9
+    frameX = (frameX + 1) % 10
     x += dirX * 10
     y += dirY * 10
     delay(0.05)

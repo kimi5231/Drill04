8a6e5764d1d3c30c3802657239fa05afa282e74b
kimi5231 <djwjod@naver.com>
2023-09-28 17:03:35 +0900

    load image

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,5 +1,8 @@
 from pico2d import*
 
-open_canvas()
+WIDTH, HEIGHT = 1280, 1024
+open_canvas(WIDTH, HEIGHT)
+background = load_image('TUK_GROUND.png')
+character = load_image('sprite_sheet.png')
 
 close_canvas()
